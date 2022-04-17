146a55a7e08120579a84706591db0a8a14b82852
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2022-04-17 16:47:22 -0300

    scripts para lujuria, gula y avaricia

--- proyecto-casi - Copy (2) - Copy/Assets/SCRIPTS/itemsInteract/mesaGula.cs
+++ proyecto-casi - Copy (2) - Copy/Assets/SCRIPTS/itemsInteract/mesaGula.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mesaGula : MonoBehaviour
+public class mesaGula : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void interact()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PecadosSingleton.instance.pecadoGula = true;
+        Destroy(gameObject);
     }
 }
+
